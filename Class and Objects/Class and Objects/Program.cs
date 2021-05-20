19eec3a427fce528c9729e9e91a943e559b03c63
Ed <edalvarez2015@gmail.com>
2021-05-20 14:17:16 -0500

    Created new BankAccount and Transaction classes

--- Class and Objects/Class and Objects/Program.cs
+++ Class and Objects/Class and Objects/Program.cs
@@ -6,7 +6,9 @@ namespace Class_and_Objects
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            BankAccount account = new BankAccount("Testing New Account", 1000);
+
+            Console.WriteLine($"Account {account.Number} was created for {account.Owner} with {account.Balance} initial balance.");
         }
     }
 }
